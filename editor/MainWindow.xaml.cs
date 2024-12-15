115aa5ad0cd7077838708bbc81366b68925ecdc3
marihachi <marihachi0620@gmail.com>
2024-12-16 00:20:50 +0900

    [editor] add labels dynamically

--- editor/MainWindow.xaml.cs
+++ editor/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace editor
 {
@@ -10,9 +12,27 @@ namespace editor
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Label> lineLabels = new List<Label>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // add lines
+            for (var i = 0; i < 20; i++)
+            {
+                var label = new Label()
+                {
+                    Name = "lineLabel" + i,
+                    Foreground = Brushes.White,
+                    FontSize = 14,
+                    Padding = new Thickness { },
+                    Content = "Line " + i,
+                };
+                label.MouseDoubleClick += Label_MouseDoubleClick;
+                instList.Items.Add(label);
+                lineLabels.Add(label);
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -32,7 +52,11 @@ namespace editor
 
         private void Label_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            MessageBox.Show("MouseDoubleClick");
+            if (sender is Label label)
+            {
+                var index = lineLabels.FindIndex(x => x == label);
+                MessageBox.Show("MouseDoubleClick index=" + index);
+            }
         }
     }
 }
